3d66ba46fc96b05971bdbfbaaab30872e3c75a5d
AKSHAT ARYA <itzakshat14@gmail.com>
2025-10-07 23:36:00 +0530

    Create The King’s Feast.cpp

---
+++ The King’s Feast.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    int max_val = arr[0];
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max_val) max_val = arr[i];
+    }
+    cout << max_val;
+    return 0;
+}
