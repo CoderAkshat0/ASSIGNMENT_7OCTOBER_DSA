3121e1478c2cd564415eaa8e608301a0f69ddd16
AKSHAT ARYA <itzakshat14@gmail.com>
2025-10-07 23:36:35 +0530

    Create The Lost Soldier.cpp

---
+++ The Lost Soldier.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    int total = n * (n + 1) / 2;
+    int sum = 0;
+    for(int i = 0; i < n; i++) sum += arr[i];
+    cout << total - sum;
+    return 0;
+}
